61f5087c50dc48c1f9ba651b15f10051ae3d0df3
vidra <ottertut@gmail.com>
2011-09-21 13:06:53 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Malmag::Application.config.secret_token = 'ec80b13767473cb521f0f4c338934c3a4f24ea0e7e10c975e5a369dd6e2457865a610ff79acec67e3e6d1b197fc0830f3e66c4ab7efed9af5418bc670a736976'
